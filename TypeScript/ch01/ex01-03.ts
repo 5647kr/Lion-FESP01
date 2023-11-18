5ceed34437cebb25258ee1c17212dd603658d385
5647kr <5647kr@gmail.com>
2023-11-18 15:29:25 +0900

    TypeScript 함수에 타입 지정

--- TypeScript/ch01/ex01-03.ts
+++ TypeScript/ch01/ex01-03.ts
@@ -1,4 +1,20 @@
 (()=>{
+  console.log("==========함수에 타입 지정==========");
+  // 매개 변수명 뒤에 지정
 
+  //리턴 타입 지정
+  // 매개변수 선언무 뒤에 지정하며, 리턴값이 없을 경우 void로 지정한다.
+
+  function getCount(count: number): string {
+    return 'count: ' + count;
+  }
+
+  console.log(getCount(10));
+  // console.log(getCount('10')); //!Error
+  // console.log(getCount(10, 20)); //!Error
+  // console.log(getCount()); //!Error
+
+  var number: string = getCount(30);
+  console.log(number)
 })();
 
