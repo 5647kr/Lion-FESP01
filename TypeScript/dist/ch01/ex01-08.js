86b581fcd3b4fc52a883776e43c9e1e1a1ab49ea
5647kr <5647kr@gmail.com>
2023-11-18 16:58:52 +0900

    TypeScript 익터센션 타입

--- TypeScript/dist/ch01/ex01-08.js
+++ TypeScript/dist/ch01/ex01-08.js
@@ -1,3 +1,16 @@
 "use strict";
 (() => {
+    console.log("==========인터섹션 타입(intersection type)==========");
+    var todo1 = {
+        title: '할일1',
+        content: '할일1'
+    };
+    var todo2 = {
+        id: 2,
+        title: '할일2',
+        content: '할일2',
+        done: true
+    };
+    console.log(todo1);
+    console.log(todo2);
 })();
