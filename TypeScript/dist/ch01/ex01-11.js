79f4be0c49b86adb86c0469afbdcc3deceb485b7
5647kr <5647kr@gmail.com>
2023-11-18 18:18:11 +0900

    TypeScript 인터페이스 사용(클래스의 타입 지정)

--- TypeScript/dist/ch01/ex01-11.js
+++ TypeScript/dist/ch01/ex01-11.js
@@ -1,3 +1,18 @@
 "use strict";
 (() => {
+    console.log("==========인터페이스 사용==========");
+    class HighSchool {
+        constructor(kor, eng) {
+            this.kor = kor;
+            this.eng = eng;
+        }
+        sum() {
+            return this.kor + this.eng;
+        }
+        avg() {
+            return this.sum() / 2;
+        }
+    }
+    const user = new HighSchool(100, 90);
+    console.log(user.sum(), user.avg());
 })();
