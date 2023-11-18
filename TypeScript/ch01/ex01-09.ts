8bb731fff0e4cd2d5b6f1ca5b3fe801d1b2314d2
5647kr <5647kr@gmail.com>
2023-11-18 17:51:42 +0900

    TypeScript 인터페이스로 객체의 타입 선언

--- TypeScript/ch01/ex01-09.ts
+++ TypeScript/ch01/ex01-09.ts
@@ -1,3 +1,17 @@
 (()=>{
+  console.log("==========인터페이스로 객체의 타입 선언==========");
 
+  interface User1 {
+    name: string,
+    age: number
+  }
+
+  //객체 생성
+  var user1: User1 = {name: '김철수', age: 30}
+
+  function printUser(user: User1): void {
+    console.log(user.name, user.age)
+
+  }
+  printUser(user1)
 })();
