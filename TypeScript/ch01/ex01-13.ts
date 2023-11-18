9f346f249972f450dc14e740cc79ca5a1dd9a1d9
5647kr <5647kr@gmail.com>
2023-11-18 18:47:19 +0900

    TypeScript 인덱스 시그니처

--- TypeScript/ch01/ex01-13.ts
+++ TypeScript/ch01/ex01-13.ts
@@ -1,4 +1,37 @@
 (()=>{
+  console.log("==========인덱스 시그니처==========");
 
+  // 회원가입
+
+  interface User {
+    name: string,
+    email: string,
+    age: number,
+    // phone?: string,
+    // address?: string,
+    // hobby?: string
+
+    // 선택적 파라미터가 수십개일경우 하나씩 ?를 넣기도 어렵다
+    // 인덱스 시그니처를 사용한다.
+    [etc: string]: string | number;
+    //기타값을 넣은 뒤 그 뒤에 타입을 지정한다.
+  }
+
+  var user1: User = {
+    name: '김철수',
+    email: 'kim@kim.com',
+    age: 30,
+    phone: '010.123.123',
+    address: '서울시 어딘가',
+    hobby: '독서'
+  }
+
+  var user2: User = {
+    name: '이영희',
+    email: 'lee@lee.com',
+    age: 35
+  }
+
+  console.log(user1, user2);
 })();
 
--- TypeScript/dist/ch01/ex01-13.js
+++ TypeScript/dist/ch01/ex01-13.js
@@ -1,3 +1,18 @@
 "use strict";
 (() => {
+    console.log("==========인덱스 시그니처==========");
+    var user1 = {
+        name: '김철수',
+        email: 'kim@kim.com',
+        age: 30,
+        phone: '010.123.123',
+        address: '서울시 어딘가',
+        hobby: '독서'
+    };
+    var user2 = {
+        name: '이영희',
+        email: 'lee@lee.com',
+        age: 35
+    };
+    console.log(user1, user2);
 })();
