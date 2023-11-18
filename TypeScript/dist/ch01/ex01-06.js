3b77d56c298abfbb9ed8e16259b98a225f6daa71
5647kr <5647kr@gmail.com>
2023-11-18 16:33:01 +0900

    TypeScript 타입 별칭

--- TypeScript/dist/ch01/ex01-06.js
+++ TypeScript/dist/ch01/ex01-06.js
@@ -1,3 +1,15 @@
 "use strict";
 (() => {
+    console.log("==========타입 별칭(type alias)==========");
+    function log3(msg) {
+        console.log(msg);
+    }
+    log3('hello');
+    log3(200);
+    var msg1 = 'world';
+    var msg2 = 300;
+    log3(msg1);
+    log3(msg2);
+    msg1 = 400;
+    log3(msg1);
 })();
