4be62ad81cdb4045fa9237c88e7a72781021d908
5647kr <5647kr@gmail.com>
2023-11-19 12:32:23 +0900

    TypeScript 숫자형 Enum 사용

--- TypeScript/ch01/ex01-20.ts
+++ TypeScript/ch01/ex01-20.ts
@@ -1,3 +1,26 @@
 (()=>{
+  console.log("==========숫자형 Enum 사용==========");
 
+  //* Enum: 상수 데이터들의 집합을 정의
+  enum ClassName {
+    JS,
+    TS, 
+    React
+  }
+
+  console.log(ClassName)
+
+  function getSchedule(subject: ClassName): string | undefined {
+    if(ClassName.JS === subject) {
+      return `js 수업은 4일 교육, 2일 프로젝트`;
+    }else if(ClassName.TS === subject) {
+      return `ts 수업은 3일 교육, 1일 프로젝트`;
+    }
+  }
+
+  const jsSchedule = getSchedule(ClassName.JS);
+  const tsSchedule = getSchedule(ClassName.TS);
+
+  console.log(jsSchedule)
+  console.log(tsSchedule)
 })();
--- TypeScript/dist/ch01/ex01-20.js
+++ TypeScript/dist/ch01/ex01-20.js
@@ -1,3 +1,24 @@
 "use strict";
 (() => {
+    console.log("==========숫자형 Enum 사용==========");
+    //* Enum: 상수 데이터들의 집합을 정의
+    let ClassName;
+    (function (ClassName) {
+        ClassName[ClassName["JS"] = 0] = "JS";
+        ClassName[ClassName["TS"] = 1] = "TS";
+        ClassName[ClassName["React"] = 2] = "React";
+    })(ClassName || (ClassName = {}));
+    console.log(ClassName);
+    function getSchedule(subject) {
+        if (ClassName.JS === subject) {
+            return `js 수업은 4일 교육, 2일 프로젝트`;
+        }
+        else if (ClassName.TS === subject) {
+            return `ts 수업은 3일 교육, 1일 프로젝트`;
+        }
+    }
+    const jsSchedule = getSchedule(ClassName.JS);
+    const tsSchedule = getSchedule(ClassName.TS);
+    console.log(jsSchedule);
+    console.log(tsSchedule);
 })();
