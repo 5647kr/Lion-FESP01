6bb13651b82f5d8be76636e4a65e4a71c1d19537
5647kr <5647kr@gmail.com>
2023-11-19 22:51:59 +0900

    JavaScript Private class fields

--- TypeScript/ch02/ex02-04.js
+++ TypeScript/ch02/ex02-04.js
@@ -1,3 +1,24 @@
 (()=>{
+  console.log("==========Private class fields==========");
 
+  // 외부에서 접근 하지 못하게 막는다.
+  //클래스의 속성명, 메서드명 앞에 #을 붙인다.
+
+  class HighSchool {
+    #kor = 0;
+    #eng = 0;
+    constructor (kor, eng) {
+      this.#kor = kor;
+      this.#eng = eng;
+    }
+    
+    sum() {
+      return this.#kor + this.#eng;
+    }
+    avg() {
+      return Math.round(this.sum() / 2);
+    }
+  }
+  var score1 = new HighSchool(100, 91)
+  console.log('평균', score1.avg());
 })();
