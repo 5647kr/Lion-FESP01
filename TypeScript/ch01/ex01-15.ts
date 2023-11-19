49dec6b61aa0221aceeb68a7449ee3907b64b16f
5647kr <5647kr@gmail.com>
2023-11-19 11:46:35 +0900

    TypeScript 인터페이스 상속 계층 구조

--- TypeScript/ch01/ex01-15.ts
+++ TypeScript/ch01/ex01-15.ts
@@ -1,4 +1,41 @@
 (function(){
+  console.log("==========인터페이스 상속 계층 구조==========");
 
+  interface list {
+    title: string,
+    content: string
+  }
+
+  interface listInfo extends list{
+    readonly id: number,
+    done?: boolean
+  }
+
+  interface listInfoTime extends listInfo {
+    createAt: Date,
+    updateAt: Date,
+  }
+
+  var todo1: list = {
+    title: '할일1',
+    content: '할일1'
+  }
+  var todo2: listInfo = {
+    id: 2,
+    title: '할일2',
+    content: '할일2'
+  }
+  var todo3: listInfoTime = {
+    id: 3,
+    title: '할일3',
+    content: '할일3',
+    done: true,
+    createAt: new Date(),
+    updateAt: new Date(),
+  }
+
+  console.log(todo1)
+  console.log(todo2)
+  console.log(todo3)
 })();
 
