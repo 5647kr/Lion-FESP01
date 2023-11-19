b3da5c6aade2a6be8e99e55b9cc304937b7db032
5647kr <5647kr@gmail.com>
2023-11-19 22:09:20 +0900

    TypeScript 자바스크립트 클래스 정의

--- TypeScript/ch02/ex02-01.js
+++ TypeScript/ch02/ex02-01.js
@@ -1,3 +1,19 @@
 (()=>{
+  console.log("==========자바스크립트 클래스 정의==========");
 
+  class HighSchool {
+    constructor (kor, eng) {
+      this.kor = kor;
+      this.eng = eng;
+    }
+    
+    sum() {
+      return this.kor + this.eng;
+    }
+    avg() {
+      return Math.round(this.sum() / 2);
+    }
+  }
+  var score1 = new HighSchool(100, 91)
+  console.log('평균', score1.avg());
 })();
