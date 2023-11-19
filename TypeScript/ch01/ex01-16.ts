b20f5ce98d8d1c393870442b64cf30a0d13a1ce7
5647kr <5647kr@gmail.com>
2023-11-19 11:56:56 +0900

    TypeScript 인터페이스 다중 상속

--- TypeScript/ch01/ex01-16.ts
+++ TypeScript/ch01/ex01-16.ts
@@ -1,5 +1,39 @@
-// 인터페이스 다중 상속
 (function(){
+  console.log("==========인터페이스 다중 상속==========");
 
+  interface listItem {
+    title: string,
+    content: string
+  }
+
+  interface list {
+    id: number,
+    title: string,
+    done: boolean
+  }
+
+  //인터페이스 다중 상속
+
+  interface listInfo extends listItem, list {
+    createAt: Date,
+    updateAt: Date,
+  }
+
+  var todo1: listItem = {
+    title: '할일1',
+    content: '할일1'
+  }
+
+  var todo2: listInfo = {
+    id: 2,
+    title: '할일2',
+    content: '할일2',
+    done: true,
+    createAt: new Date(),
+    updateAt: new Date(),
+  }
+
+  console.log(todo1)
+  console.log(todo2)
 })();
 
--- TypeScript/dist/ch01/ex01-16.js
+++ TypeScript/dist/ch01/ex01-16.js
@@ -1,4 +1,18 @@
 "use strict";
-// 인터페이스 다중 상속
 (function () {
+    console.log("==========인터페이스 다중 상속==========");
+    var todo1 = {
+        title: '할일1',
+        content: '할일1'
+    };
+    var todo2 = {
+        id: 2,
+        title: '할일2',
+        content: '할일2',
+        done: true,
+        createAt: new Date(),
+        updateAt: new Date(),
+    };
+    console.log(todo1);
+    console.log(todo2);
 })();
