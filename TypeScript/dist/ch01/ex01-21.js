e3086b4485f7106f3ee215913feb92d905e2ab1c
5647kr <5647kr@gmail.com>
2023-11-19 12:35:37 +0900

    TypeScript 문자형 Enum 사용

--- TypeScript/dist/ch01/ex01-21.js
+++ TypeScript/dist/ch01/ex01-21.js
@@ -1,4 +1,24 @@
 "use strict";
-// ex01-20.ts 복사
 (() => {
+    console.log("==========문자형 Enum 사용==========");
+    //* Enum: 상수 데이터들의 집합을 정의
+    let ClassName;
+    (function (ClassName) {
+        ClassName["JS"] = "javascript";
+        ClassName["TS"] = "typescript";
+        ClassName["React"] = "react";
+    })(ClassName || (ClassName = {}));
+    console.log(ClassName);
+    function getSchedule(subject) {
+        if (ClassName.JS === subject) {
+            return `${ClassName.JS} 수업은 4일 교육, 2일 프로젝트`;
+        }
+        else if (ClassName.TS === subject) {
+            return `${ClassName.TS} 수업은 3일 교육, 1일 프로젝트`;
+        }
+    }
+    const jsSchedule = getSchedule(ClassName.JS);
+    const tsSchedule = getSchedule(ClassName.TS);
+    console.log(jsSchedule);
+    console.log(tsSchedule);
 })();
