ad486a025db4aecd07e1d48f6ce612bf0871ef7b
5647kr <5647kr@gmail.com>
2023-11-19 12:25:56 +0900

    TypeScript 상수 그룹화

--- TypeScript/ch01/ex01-19-01.ts
+++ TypeScript/ch01/ex01-19-01.ts
@@ -1,3 +1,28 @@
 (()=>{
+  console.log("==========상수 그룹화==========");
 
+  type ReadOnly = {
+    readonly JS: string,
+    readonly TS: string,
+    readonly React: string
+  }
+
+  const ClassName: ReadOnly = {
+    JS: 'javascript',
+    TS: 'typescript',
+    React: 'react'
+  }
+  function getSchedule(subject: string): string | undefined {
+    if(ClassName.JS === subject) {
+      return `js 수업은 4일 교육, 2일 프로젝트`;
+    }else if(ClassName.TS === subject) {
+      return `ts 수업은 3일 교육, 1일 프로젝트`;
+    }
+  }
+
+  const jsSchedule = getSchedule(ClassName.JS);
+  const tsSchedule = getSchedule(ClassName.TS);
+
+  console.log(jsSchedule)
+  console.log(tsSchedule)
 })();
--- TypeScript/dist/ch01/ex01-19-01.js
+++ TypeScript/dist/ch01/ex01-19-01.js
@@ -1,3 +1,21 @@
 "use strict";
 (() => {
+    console.log("==========상수 그룹화==========");
+    const ClassName = {
+        JS: 'javascript',
+        TS: 'typescript',
+        React: 'react'
+    };
+    function getSchedule(subject) {
+        if (ClassName.JS === subject) {
+            return `js 수업은 4일 교육, 2일 프로젝트`;
+        }
+        else if (ClassName.TS === subject) {
+            return `ts 수업은 3일 교육, 1일 프로젝트`;
+        }
+    }
+    const jsSchedule = getSchedule(ClassName.JS);
+    const tsSchedule = getSchedule(ClassName.TS);
+    console.log(jsSchedule);
+    console.log(tsSchedule);
 })();
