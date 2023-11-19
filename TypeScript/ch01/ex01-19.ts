57871e6babd1a52e541025f2438b270ccc57e29b
5647kr <5647kr@gmail.com>
2023-11-19 12:21:17 +0900

    TypeScript 상수 사용

--- TypeScript/ch01/ex01-19.ts
+++ TypeScript/ch01/ex01-19.ts
@@ -1,3 +1,20 @@
 (()=>{
+  console.log("==========상수 사용==========");
 
+  const js = 'javascript'
+  const ts = 'typsscript'
+
+  function getSchedule(subject: string): string | undefined {
+    if('js' === subject) {
+      return `js 수업은 4일 교육, 2일 프로젝트`;
+    }else if('ts' === subject) {
+      return `ts 수업은 3일 교육, 1일 프로젝트`;
+    }
+  }
+
+  const jsSchedule = getSchedule(js);
+  const tsSchedule = getSchedule(ts);
+
+  console.log(jsSchedule)
+  console.log(tsSchedule)
 })();
