c6ae1f48aa601c01a6c0a1f7ef4e604d82968d97
5647kr <5647kr@gmail.com>
2023-11-19 12:06:19 +0900

    TypeScript 인터페이스 재선언(선언 병합)

--- TypeScript/dist/ch01/ex01-17.js
+++ TypeScript/dist/ch01/ex01-17.js
@@ -1,3 +1,20 @@
 "use strict";
 (function () {
+    console.log("==========인터페이스 재선언(선언 병합)==========");
+    var todo1 = {
+        id: 1,
+        title: '할일1',
+        content: '할일1',
+        done: true
+    };
+    console.log(todo1);
+    //!Error
+    // type list2 = {
+    //   id: number,
+    //   done: boolean
+    // }
+    // type list2 = {
+    //   title: string,
+    //   content: string
+    // }
 })();
