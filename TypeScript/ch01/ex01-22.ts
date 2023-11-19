b4a88ddb0336b0aa8b1e148334fd45cf40557896
5647kr <5647kr@gmail.com>
2023-11-19 12:38:25 +0900

    TypeScript const Enum

--- TypeScript/ch01/ex01-22.ts
+++ TypeScript/ch01/ex01-22.ts
@@ -1,4 +1,26 @@
-// ex01-21.ts 복사
 (()=>{
+  console.log("==========const Enum==========");
 
+  //* Enum: 상수 데이터들의 집합을 정의
+  const enum ClassName {
+    JS = 'javascript',
+    TS = 'typescript', 
+    React = 'react'
+  }
+
+  console.log(ClassName)
+
+  function getSchedule(subject: ClassName): string | undefined {
+    if(ClassName.JS === subject) {
+      return `${ClassName.JS} 수업은 4일 교육, 2일 프로젝트`;
+    }else if(ClassName.TS === subject) {
+      return `${ClassName.TS} 수업은 3일 교육, 1일 프로젝트`;
+    }
+  }
+
+  const jsSchedule = getSchedule(ClassName.JS);
+  const tsSchedule = getSchedule(ClassName.TS);
+
+  console.log(jsSchedule)
+  console.log(tsSchedule)
 })();
--- TypeScript/dist/ch01/ex01-22.js
+++ TypeScript/dist/ch01/ex01-22.js
@@ -1,4 +1,17 @@
 "use strict";
-// ex01-21.ts 복사
 (() => {
+    console.log("==========const Enum==========");
+    console.log(ClassName);
+    function getSchedule(subject) {
+        if ("javascript" /* ClassName.JS */ === subject) {
+            return `${"javascript" /* ClassName.JS */} 수업은 4일 교육, 2일 프로젝트`;
+        }
+        else if ("typescript" /* ClassName.TS */ === subject) {
+            return `${"typescript" /* ClassName.TS */} 수업은 3일 교육, 1일 프로젝트`;
+        }
+    }
+    const jsSchedule = getSchedule("javascript" /* ClassName.JS */);
+    const tsSchedule = getSchedule("typescript" /* ClassName.TS */);
+    console.log(jsSchedule);
+    console.log(tsSchedule);
 })();
