76a4d2d78dae445e6a22a86aabd0c312146e19b8
5647kr <5647kr@gmail.com>
2025-04-04 20:54:52 +0900

    JavaScript 화살표 함수

--- JavaScript/ch02/ex02-10 화살표 함수.js
+++ JavaScript/ch02/ex02-10 화살표 함수.js
@@ -11,26 +11,26 @@ console.log(arr2);
 
 console.log("----------화살표 함수 생성2----------");
 // ECMA5
-var arr2 = [];
+var arr3 = [];
 arr.forEach(function(elem){
-  arr2.push(elem ** 2);
+  arr3.push(elem ** 2);
 });
-console.log(arr2);
+console.log(arr3);
 
 console.log("----------화살표 함수 생성3----------");
 // ECMA6
-var arr2 = arr.map(function(elem){
+var arr4 = arr.map(function(elem){
   return elem ** 2;
 });
-console.log(arr2);
+console.log(arr4);
 
 console.log("----------화살표 함수 생성4----------");
 // ECMA6 arrow function
-var arr2 = [];
+var arr5 = [];
 arr.forEach((elem) => {
-  arr2.push(elem ** 2);
+  arr5.push(elem ** 2);
 });
-console.log(arr2);
+console.log(arr5);
 
 //* 요소가 하나일 경우 () 생략할 수 있다.
 // var arr2 = arr.map(elem => {
