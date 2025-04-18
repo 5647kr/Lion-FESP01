4c694182ce73ff0423c35dc5300fb8bbeb71e263
5647kr <5647kr@gmail.com>
2025-04-18 14:06:11 +0900

    JavaScript 배열 프로퍼티 생성

--- JavaScript/ch01/ex01-13 배열 프로퍼티 생성.js
+++ JavaScript/ch01/ex01-13 배열 프로퍼티 생성.js
@@ -1,7 +1,8 @@
+console.log("========== 배열 프로퍼티 생성 ==========");
 // 지정한 배열의 모든 요소를 출력한다.
 function printArr(arr){
   for(var i=0; i<arr.length; i++){
-    console.log(i, arr[i]);
+    console.log(`i: ${i} / arr[${i}]: ${arr[i]}`);
   }
 }
 
