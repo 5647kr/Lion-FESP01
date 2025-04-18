e1b4894bc70a951afb594f409e8f76a8381f131b
5647kr <5647kr@gmail.com>
2025-04-18 14:06:30 +0900

    JavaScript 함수

--- JavaScript/ch01/ex01-16 함수.js
+++ JavaScript/ch01/ex01-16 함수.js
@@ -1,10 +1,11 @@
-console.log("----------함수----------");
+console.log("========== 함수 ==========");
 
 // TODO: hoisting 단원. 함수 선언보다 먼저 사용이 가능한 이유
-console.log("----------hoisting----------");
+console.log("----- hoisting -----");
 sum1();
 
-console.log("----------함수1----------");
+
+console.log("----- 함수1 -----");
 
 // 10 + 100의 결과를 출력한다.
 function sum1(){
@@ -15,7 +16,8 @@ function sum1(){
 }
 sum1();
 
-console.log("----------함수2----------");
+
+console.log("----- 함수2 -----");
 
 // 전달받은 숫자와 100의 합계를 출력한다.
 function sum2(n1){
@@ -26,7 +28,8 @@ function sum2(n1){
 sum2(20);
 sum2(30);
 
-console.log("----------함수3----------");
+
+console.log("----- 함수3 -----");
 
 // 전달받은 두 수의 합계를 출력한다.
 function sum3(n1, n2){
@@ -36,7 +39,8 @@ function sum3(n1, n2){
 sum3(30, 40);
 sum3(30, 40);
 
-console.log("----------함수4----------");
+
+console.log("----- 함수4 -----");
 
 // 전달받은 두 수의 합계를 반환한다.
 function sum4(n1, n2){
