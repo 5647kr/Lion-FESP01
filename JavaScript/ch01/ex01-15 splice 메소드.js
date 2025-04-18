92f7a546d3d74cb51bf111a33e83c60ec8b636ea
5647kr <5647kr@gmail.com>
2025-04-18 14:06:23 +0900

    JavaScript splice 메소드

--- JavaScript/ch01/ex01-15 splice 메소드.js
+++ JavaScript/ch01/ex01-15 splice 메소드.js
@@ -1,24 +1,25 @@
-console.log("----------splice 연산자----------");
+console.log("========== splice 연산자 ==========");
 
 // TODO: hoisting 단원. 변수 중복 선언 가능
 var arr1 = ['zero', 'one', 'three', 'four'];
 var arr2 = arr1.splice(2, 0, 'two');
-console.log('arr1', arr1);
+console.log('arr1:', arr1);
 
 // splice(start[, deleteCount[, item1[, item2[, ...]]]])
 // 배열의 요소를 삭제, 교체, 추가하고 제거한 요소를 담은 배열을 반환한다.
 // deleteCount 생략시 마지막까지 제거
 
 var arr2 = arr1.splice(3, 1);
-console.log(arr2);
+console.log("arr1.splice(3, 1):", arr2);
 
 var arr2 = arr1.splice(1);
-console.log(arr2);
+console.log("arr1.splice(1):", arr2);
 
 var arr2 = arr1.splice();  // 빈배열 반환
-console.log(arr2);
+console.log("arr1.splice():", arr2);
 
-console.log("----------slice 연산자----------");
+
+console.log("========== slice 연산자 ==========");
 
 var arr3 = ['zero', 'one', 'three', 'four'];
 var arr4 = arr3.splice(2, 0, 'two');
@@ -32,16 +33,21 @@ var arr4 = arr3.splice(2, 0, 'two');
 // 원본 데이터 유지
 
 var arr4 = arr3.slice(1, 3);
-console.log(arr4);
-var arr4 = arr3.slice(-3, -1);
-console.log(arr4);
-var arr4 = arr3.slice(2);
-console.log(arr4);
-var arr4 = arr3.slice(-2);
-console.log(arr4);
-var arr4 = arr3.slice(); // 전체를 복사한다.
-console.log(arr4);
+console.log("arr3.slice(1, 3):", arr4);
 
-console.log('arr3', arr3);
-console.log('arr4', arr4);
-console.log(arr3 === arr4);
+var arr4 = arr3.slice(-3, -1);
+console.log("arr3.slice(-3, -1):", arr4);
+
+var arr4 = arr3.slice(2);
+console.log("arr3.slice(2):", arr4);
+
+var arr4 = arr3.slice(-2);
+console.log("arr3.slice(-2):", arr4);
+
+var arr4 = arr3.slice(); // 전체를 복사한다.
+console.log("arr3.slice():", arr4);
+
+
+console.log('arr3:', arr3);
+console.log('arr4:', arr4);
+console.log('arr3 === arr4:', arr3 === arr4);
