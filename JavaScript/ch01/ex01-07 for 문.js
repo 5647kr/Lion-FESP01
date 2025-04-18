71ba128cbe4f8696f1873d3be04459630ebb7dc3
5647kr <5647kr@gmail.com>
2025-04-18 14:05:28 +0900

    JavaScript for문

--- JavaScript/ch01/ex01-07 for 문.js
+++ JavaScript/ch01/ex01-07 for 문.js
@@ -1,32 +1,54 @@
 //* 객체의 모든 속성 접근
 
+console.log("========== for in 구문 ==========");
 // for in 구문
 // for (var prop in obj) {};
+
+console.log(`for (var prop in obj) {};`)
 
 var obj1 = {
   name: 'obj1',
   major: 'computer science'
 };
 
-// 객체 프로퍼티 읽기
-console.log(obj1.name.toUpperCase(), obj1['major'], obj1.sub/*.toUpperCase()*/);
-// toUpperCase(): 대문자로
 
+console.log("----- 객체 프로퍼티 읽기 -----");
+// 객체 프로퍼티 읽기
+console.log("obj1.name:", obj1.name);
+console.log("obj1['major']:", obj1['major']);
+console.log("obj1.sub:", obj1.sub);
+
+
+console.log("----- 객체 프로퍼티 갱신 -----");
 // 객체 프로퍼티 갱신
 obj1.major = 'electronic engineering';
-console.log(obj1.name.toUpperCase(), obj1['major'], obj1.sub/*.toUpperCase()*/);
 
+console.log("obj1.name:", obj1.name);
+console.log("obj1['major']:", obj1['major']);
+console.log("obj1.sub:", obj1.sub);
+
+
+console.log("----- 객체 프로퍼티 동적 생성 -----");
 // 객체 프로퍼티 동적 생성
 obj1.sub = 'computer engineering';
-console.log(obj1.name.toUpperCase(), obj1['major'], obj1.sub/*.toUpperCase()*/);
 
+console.log("obj1.name:", obj1.name);
+console.log("obj1['major']:", obj1['major']);
+console.log("obj1.sub:", obj1.sub);
+
+
+console.log("----- 객체 프로퍼티 삭제 -----");
 // 객체 프로퍼티 삭제
 delete obj1.major;
-console.log(obj1.name.toUpperCase(), obj1['major'], obj1.sub/*.toUpperCase()*/);
+console.log("obj1.name:", obj1.name);
+console.log("obj1['major']:", obj1['major']);
+console.log("obj1.sub:", obj1.sub);
 
+
+console.log("----- 객체 모든 프로퍼티 읽기 -----");
 // 객체 모든 프로퍼티 읽기
 for(var prop in obj1){
-  console.log(prop, obj1[prop]);
+  console.log("prop:", prop, "/ obj1[prop]:", obj1[prop]);
 }
 
 
