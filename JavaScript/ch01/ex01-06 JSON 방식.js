21f60fe5e13bca9dba8283e9f7baa18bc4108354
5647kr <5647kr@gmail.com>
2025-04-18 14:05:21 +0900

    JavaScript JSON표기방법

--- JavaScript/ch01/ex01-06 JSON 방식.js
+++ JavaScript/ch01/ex01-06 JSON 방식.js
@@ -1,4 +1,5 @@
 //* JSON 표기방법
+console.log("========== JSON 표기방법 ==========");
 
 var obj1 = {
   name: 'lee',
@@ -8,14 +9,26 @@ var obj1 = {
 };
 
 //! 속성명에 '', ""를 붙이지 않아도 된다.
+console.log("type:", typeof obj1, "/ value:", obj1);
+console.log("obj1['name']:", obj1['name']);
+console.log("obj1.age:", obj1.age);
+console.log("obj1.job:", obj1.job);
+console.log("obj1.['married']:", obj1['married']);
 
-console.log(obj1['name'], obj1.age, obj1.job, obj1['married']);
-console.log(typeof obj1, obj1);
 
+console.log("----- 객체 -> 문자열 -----");
 // 객체를 문자열로 변환
-var strObj1 = JSON.stringify(obj1);
-console.log("strObj:", typeof strObj1, strObj1, strObj1.age);
+var objStr = JSON.stringify(obj1);
 
+console.log("type:", typeof objStr);
+console.log("objStr:", objStr);
+console.log("objStr.age:", objStr.age);
+
+
+console.log("----- 문자열 -> 객체 -----");
 //문자열을 객체로 변환
-var objObj1 = JSON.parse(strObj1);
-console.log("objObj:", typeof objObj1, objObj1, objObj1.age);
+var strObj = JSON.parse(objStr);
+
+console.log("type:", typeof strObj);
+console.log("strObj:", strObj);
+console.log("strObj.age:", strObj.age);
