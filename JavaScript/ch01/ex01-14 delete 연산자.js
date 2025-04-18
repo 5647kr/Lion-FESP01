7838eba9d03c3498690336f4b409a2897c8e8002
5647kr <5647kr@gmail.com>
2025-04-18 14:06:17 +0900

    JavaScript delete 연산자

--- JavaScript/ch01/ex01-14 delete 연산자.js
+++ JavaScript/ch01/ex01-14 delete 연산자.js
@@ -1,9 +1,8 @@
-console.log("----------delete 연산자----------");
-
+console.log("========== delete 연산자 ==========");
 // 지정한 배열의 모든 요소를 출력한다.
 function printArr(arr){
   for(var i=0; i<arr.length; i++){
-    console.log(i, arr[i]);
+    console.log(`i: ${i} / arr[${i}]: ${arr[i]}`);
   }
 }
 
