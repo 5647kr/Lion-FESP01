e07cec6b8384247cfe93bf46a7dc653fb934d169
5647kr <5647kr@gmail.com>
2025-04-19 15:42:14 +0900

    JavaScript 익명 함수

--- JavaScript/ch02/ex02-06 익명 함수.js
+++ JavaScript/ch02/ex02-06 익명 함수.js
@@ -1,9 +1,10 @@
-console.log("----------표현식 함수 생성----------");
+console.log("========== 함수 선언 ==========");
 
-console.log("----------익명 함수----------");
+console.log("----- 익명 함수 -----");
 
-var add = function (a, b) {
+var fuc2 = function (a, b) {
   var result = a + b;
   return result;
 };
-console.log(add(30, 40));
+
+console.log("fuc2(30, 40):", fuc2(30, 40));
