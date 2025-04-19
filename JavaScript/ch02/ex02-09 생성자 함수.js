2f59c4a4b91b8c5063f6047e7b3663dfc8f65fdb
5647kr <5647kr@gmail.com>
2025-04-19 15:42:28 +0900

    JavaScript 생성자 함수

--- JavaScript/ch02/ex02-09 생성자 함수.js
+++ JavaScript/ch02/ex02-09 생성자 함수.js
@@ -1,6 +1,6 @@
-console.log("----------Function 함수 생성----------");
+console.log("========== Function 생성자 함수 ==========");
 
 // Function 생성자 함수
-var add = new Function('x', 'y', 'return x + y;');
+var fuc1 = new Function('x', 'y', 'return x + y;');
 //! 반드시 function을 대문자로 
-console.log(add(50, 60));
+console.log("fuc1(50, 60):", fuc1(50, 60));
