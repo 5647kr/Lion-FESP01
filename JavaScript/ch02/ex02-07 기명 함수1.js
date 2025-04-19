61341beae34dadc0b9dc71fe894457d686176641
5647kr <5647kr@gmail.com>
2025-04-19 15:42:22 +0900

    JavaScript 기명 함수

--- JavaScript/ch02/ex02-07 기명 함수1.js
+++ JavaScript/ch02/ex02-07 기명 함수1.js
@@ -1,10 +1,12 @@
-console.log("----------기명 함수1----------");
+console.log("========== 함수 선언 ==========");
+
+console.log("----- 기명 함수 -----");
 // 표현식 방식의 함수 생성(기명함수)
 
-let add1 = function sum1(a, b) {
+let fuc1 = function sum1(a, b) {
   return a + b;
 };
 
-console.log(add1(30, 40));
+console.log("fuc1(30, 40):", fuc1(30, 40));
 //console.log(sum1(30, 40)); //!Error
 
--- JavaScript/ch02/ex02-08 기명 함수2.js
+++ JavaScript/ch02/ex02-08 기명 함수2.js
@@ -1,12 +1,15 @@
-console.log("----------기명 함수2----------");
+console.log("========== 함수 선언 ==========");
+
+console.log("----- 기명 함수 -----");
 
 // 표현식 방식의 함수 생성(기명함수 사용예)
-var f = function factorial(n){
+// 함수 내부에서 사용은 가능하다 즉, 재귀함수로 사용할 때 사용한다.
+var fuc1 = function factorial(n){
 	if(n == 1) return 1;
   return n * factorial(n - 1);
 };
 
-console.log(f(5));
+console.log("fuc1(5):", fuc1(5));
 // console.log(factorial(5)); //! Error
 
 // 5! -> 5 * 4 * 3 * 2 * 1
@@ -16,7 +19,7 @@ console.log(f(5));
 // 5 * 4 * 3 * 2 * 1!
 // 5 * 4 * 3 * 2 * 1
 
-console.log('로또 1등 당첨 확률은 1/' + Math.round(f(45)/(f(6)*f(39))));
+console.log('로또 1등 당첨 확률은 1/' + Math.round(fuc1(45) / (fuc1(6) * fuc1(39))));
 
 // 함수안에서 자기 자신을 부르는 함수를 재귀함수라 한다.
 
