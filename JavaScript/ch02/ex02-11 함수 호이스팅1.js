fea78a1f7863bdce19ac0e94b44562546ed9594a
5647kr <5647kr@gmail.com>
2025-04-19 15:42:45 +0900

    JavaScript 함수 호이스팅

--- JavaScript/ch02/ex02-11 함수 호이스팅1.js
+++ JavaScript/ch02/ex02-11 함수 호이스팅1.js
@@ -1,14 +1,14 @@
-console.log("----------함수 호이스팅1----------");
+console.log("========== 함수 호이스팅1 ==========");
 
 // 선언문 형태(선언문 형식의 함수, var)로 정의한 함수의 유효범위는 코드의 맨 처음부터 시작한다.
 
-console.log(add(10, 20));
+console.log(fuc1(10, 20));
 
 // 선언문 형식의 함수 정의
-function add(x, y){
+function fuc1(x, y){
   return x + y;
 }
 
-console.log(add(10, 20));
+console.log(fuc1(10, 20));
 
 
--- JavaScript/ch02/ex02-12 함수 호이스팅2.js
+++ JavaScript/ch02/ex02-12 함수 호이스팅2.js
@@ -1,12 +1,12 @@
-console.log("----------함수 호이스팅2----------");
+console.log("========== 함수 호이스팅2 ==========");
 
-// console.log(add(10, 20)); //!Error
+// console.log(fuc1(10, 20)); //!Error
 
 // 선언문 형식의 함수 정의
-var add = function (x, y){
+var fuc1 = function (x, y){
   return x + y;
 }
 
-console.log(add(10, 20));
+console.log(fuc1(10, 20));
 
 // -> ch01-15, ch01-16, ch01-22
