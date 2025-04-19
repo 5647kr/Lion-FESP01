cda59131b151d76790922f15c8a49060b701775f
5647kr <5647kr@gmail.com>
2025-04-19 15:42:08 +0900

    JavaScript 함수 선언

--- JavaScript/ch02/ex02-05 함수 선언.js
+++ JavaScript/ch02/ex02-05 함수 선언.js
@@ -1,14 +1,17 @@
-console.log("----------this(apply, call)----------");
+console.log("========== 함수 선언 ==========");
 
 // 선언문 방식의 함수 생성
-function add(x, y){
+function fuc1(x, y){
   return x + y;
 }
 
-console.log(add(10, 20));
+console.log("fuc1(10, 20):", fuc1(10, 20));
 
 
-console.log(add.call(this, 10, 20));
-console.log(add.apply(this, [10, 20]));
+// this (Ch02-14)
+console.log("fuc1.call(this, 10, 20):", fuc1.call(this, 10, 20));
+console.log("fuc1.apply(this, [10, 20]):", fuc1.apply(this, [10, 20]));
+// console.log("fuc1.apply(this, 10, 20):", fuc1.apply(this, 10, 20)); // Error
 
 
+// Ch02-15 ->
