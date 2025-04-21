17b6df447e90cb9d37ef742fcd2f08f0a8fa52d6
5647kr <5647kr@gmail.com>
2025-04-21 20:21:00 +0900

    JavaScript 가변인자

--- JavaScript/ch02/ex02-16 가변인자.js
+++ JavaScript/ch02/ex02-16 가변인자.js
@@ -1,8 +1,8 @@
 console.log("========== 가변인자 ==========");
 
-console.log(Math.min(10,100));	// 10
-console.log(Math.min(200,100,50,60,80,30));//30
-console.log(Math.min(100,20,60,50,70)); // 20
+// console.log(Math.min(10,100));	// 10
+// console.log(Math.min(200,100,50,60,80,30));//30
+// console.log(Math.min(100,20,60,50,70)); // 20
 
 // 지정한 배열의 요소 중 최소값을 반환
 function smallest(nums){
@@ -28,6 +28,6 @@ function smallest(nums){
 
 // ../js/mylib.js 으로
 
-console.log(smallest([10,100]));	// 10
-console.log(smallest([200,100,50,60,80,30]));//30
-console.log(smallest(100,20,60,50,70));	// 20
+console.log("smallest():", smallest([10,100]));	// 10
+console.log("smallest():", smallest([200,100,50,60,80,30]));//30
+console.log("smallest():", smallest(100,20,60,50,70));	// 20
