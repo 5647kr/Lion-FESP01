f217ba64fad85fdc571b1f5d2e78dd68887f1b4b
5647kr <5647kr@gmail.com>
2025-04-21 20:21:52 +0900

    JavaScript 커링

--- JavaScript/ch04/ex04-03 커링1.js
+++ JavaScript/ch04/ex04-03 커링1.js
@@ -1,7 +1,7 @@
 function sum(a, b, c){
   return a + b + c;
 }
-console.log(sum(10, 20, 30));
+console.log("sum(10, 20, 30):", sum(10, 20, 30));
 
 function currySum1(a) {
   return function (b) {
@@ -24,14 +24,14 @@ var curryfuc1 = function (fn) {
 };
 var currySum3 = curryfuc1(sum)
 
-console.log(currySum1(10)(20)(30));
-console.log(currySum2(10)(20)(30));
+console.log("currySum1(10)(20)(30):", currySum1(10)(20)(30));
+console.log("currySum2(10)(20)(30):", currySum2(10)(20)(30));
 
 
 // lodash
 
 var currySum4 = _.curry(sum);
-console.log(currySum4(2)(3)(4));
-console.log(currySum4(2, 3)(4));
-console.log(currySum4(2) (3, 4));
-console.log(currySum4(2, 3, 4));
+console.log("currySum4(2)(3)(4):", currySum4(2)(3)(4));
+console.log("currySum4(2, 3)(4):", currySum4(2, 3)(4));
+console.log("currySum4(2)(3, 4):", currySum4(2)(3, 4));
+console.log("currySum4(2, 3, 4):", currySum4(2, 3, 4));
