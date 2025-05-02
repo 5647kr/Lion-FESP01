548104820af5519aca1f2036656db03d13aad23c
5647kr <5647kr@gmail.com>
2025-05-02 12:00:00 +0900

    TypeScript 선택적 파라미터

---
+++ TypeScript/ch01/ex01-04 선택적 파라미터.ts
@@ -0,0 +1,21 @@
+(() => {
+  console.log("========== 선택적 파라미터(optional para) ==========");
+
+  // 함수의 매개 변수를 선택적으로 전달 받고 싶을 때 매개변수명 뒤 ?를 추가한다.
+
+  function user1(name: string, age: number): void {
+    console.log("user1.name", name);
+    console.log("user1.age", age);
+  }
+
+  user1("김철수", 30);
+  // user1('이영희') //!Error
+
+  function user2(name: string, age?: number): void {
+    console.log("user2.name", name);
+    console.log("user2.age", age);
+  }
+
+  user2("김철수", 30);
+  user2("이영희");
+})();
