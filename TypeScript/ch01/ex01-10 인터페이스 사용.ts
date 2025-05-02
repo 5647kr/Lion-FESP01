c82d189a5686f2265a086a42ea04790632cdc0d8
5647kr <5647kr@gmail.com>
2025-05-02 12:00:00 +0900

    TypeScript 인터페이스 사용

---
+++ TypeScript/ch01/ex01-10 인터페이스 사용.ts
@@ -0,0 +1,29 @@
+(() => {
+  console.log("========== 인터페이스 사용 ==========");
+  //1. 변수, 함수의 매개변수, 함수의 리턴 타입
+
+  interface User1 {
+    name: string;
+    age: number;
+  }
+
+  //2. 변수에 타입 지정
+  var user1: User1 = { name: "김철수", age: 30 };
+  console.log("user1:", user1);
+
+  //3. 함수 매개 변수에 타입 지정
+  var getAge = function(user1: User1) {
+    return user1.age;
+  };
+
+
+  //4. 함수의 리턴 타입 지정
+  var createUser = function(name: string, age: number): User1 {
+    return { name, age };
+  };
+
+  // 타입추론에 의해 자동으로 타입이 할당됨으로 따로 변수에 타입을 지정하지 않아도 된다.
+  var kim: User1 = createUser("이경이", 35);
+  //변수까지 타입 지정은 하지 않아도 된다.
+  console.log(getAge(kim));
+})();
