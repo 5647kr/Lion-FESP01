5c9a7d4884c0e3a8b4424a0a2a6596356e307bfe
5647kr <5647kr@gmail.com>
2025-05-02 12:00:00 +0900

    TypeScript 인터페이스 사용

---
+++ TypeScript/ch01/ex01-11 인터페이스 사용.ts
@@ -0,0 +1,34 @@
+(()=>{
+  console.log("========== 인터페이스 사용 ==========");
+
+  // 클래스의 타입 지정
+  // 클래스명 뒤에 implements 키워드를 추가한다.
+
+  interface Score {
+    kor: number,
+    eng: number,
+    sum(): number,
+    avg(): number
+  }
+
+  class HighSchool implements Score {
+    kor: number;
+    eng: number;
+    constructor(kor: number, eng: number) {
+      this.kor = kor;
+      this.eng = eng;
+    }
+    sum() {
+      return this.kor + this.eng;
+    }
+
+    avg() {
+      return this.sum() / 2;
+    }
+  }
+
+  const user = new HighSchool(100, 90)
+  console.log("user.sum():", user.sum());
+  console.log("user.avg():", user.avg());
+})();
+
