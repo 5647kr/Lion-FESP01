3305346b6d92e536f0ea55088b9ea7e7243aca0d
5647kr <5647kr@gmail.com>
2025-05-02 12:00:00 +0900

    개발 환경 확인

---
+++ TypeScript/ch01/ex01-01 개발 환경 확인.ts
@@ -0,0 +1,9 @@
+(() => {
+  console.log("========== 개발 환경 확인 ==========");
+  function hello(name: string): string {
+    // 매개변수 뒤에 :타입명     return 타입을 지정한다.
+    return "hello" + name;
+  }
+  console.log(hello("TypeScript!"));
+  // 자동 컴파일 명령어: tsc --w
+})();
