bf5ba492e5e3b7ba3da92d96a1928a6ee1a66486
5647kr <5647kr@gmail.com>
2025-05-03 12:00:00 +0900

    TypeScript private 접근 지정자

--- TypeScript/ch02/ex02-03 private 접근 지정자.ts
+++ TypeScript/ch02/ex02-03 private 접근 지정자.ts
@@ -1,5 +1,5 @@
-(()=>{
-  console.log("==========private 접근 지정자==========");
+(() => {
+  console.log("========== private 접근 지정자 ==========");
 
   interface Score {
     avg(): number;
@@ -8,21 +8,26 @@
   class HighSchool implements Score {
     //1. 맴버 변수 타입을 정의
     //2. 생성자 메서드의 매개변수와 리턴 타입을 정의
-    private kor: number; 
-    private eng: number; 
+    private kor: number;
+    private eng: number;
 
-    constructor (kor: number, eng: number) {
+    constructor(kor: number, eng: number) {
       this.kor = kor;
       this.eng = eng;
     }
-    
+
     private sum(): number {
       return this.kor + this.eng;
     }
+
     avg(): number {
       return Math.round(this.sum() / 2);
     }
   }
-  var score1 = new HighSchool(100, 91)
-  console.log('평균', score1.avg());
+  var score1 = new HighSchool(100, 91);
+  console.log("평균:", score1.avg());
+  
+  // 잘못된 사용
+  // score1.eng = 99;
+  // console.log("평균:", score1.avg());
 })();
