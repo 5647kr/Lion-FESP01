db8cdcb6fc71728e59811580e012241f8af1faff
5647kr <5647kr@gmail.com>
2025-05-03 12:00:00 +0900

    TypeScript 문자형 Enum

---
+++ TypeScript/ch01/ex01-21 문자형 Enum.ts
@@ -0,0 +1,26 @@
+(() => {
+  console.log("========== 문자형 Enum 사용 ==========");
+
+  //* Enum: 상수 데이터들의 집합을 정의,
+  enum ClassName {
+    JS = "javascript",
+    TS = "typescript",
+    React = "react",
+  }
+
+  console.log("ClassName:", ClassName);
+
+  function getSchedule(subject: ClassName): string | undefined {
+    if (ClassName.JS === subject) {
+      return `${ClassName.JS} 수업은 4일 교육, 2일 프로젝트`;
+    } else if (ClassName.TS === subject) {
+      return `${ClassName.TS} 수업은 3일 교육, 1일 프로젝트`;
+    }
+  }
+
+  const jsSchedule = getSchedule(ClassName.JS);
+  const tsSchedule = getSchedule(ClassName.TS);
+
+  console.log("jsSchedule:", jsSchedule);
+  console.log("tsSchedule:", tsSchedule);
+})();
