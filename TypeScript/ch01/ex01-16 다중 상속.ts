369e4f9aab12f053b3e7621463eb3aec582073cd
5647kr <5647kr@gmail.com>
2025-05-03 12:00:00 +0900

    TypeScript 다중 상속

--- TypeScript/ch01/ex01-16 다중 상속.ts
+++ TypeScript/ch01/ex01-16 다중 상속.ts
@@ -1,12 +1,12 @@
-(function(){
+(() => {
   console.log("==========인터페이스 다중 상속==========");
 
-  interface listItem {
+  interface List1 {
     title: string,
     content: string
   }
 
-  interface list {
+  interface List2 {
     id: number,
     title: string,
     done: boolean
@@ -14,17 +14,17 @@
 
   //인터페이스 다중 상속
 
-  interface listInfo extends listItem, list {
+  interface List3 extends List1, List2 {
     createAt: Date,
     updateAt: Date,
   }
 
-  var todo1: listItem = {
+  var todo1: List1 = {
     title: '할일1',
     content: '할일1'
   }
 
-  var todo2: listInfo = {
+  var todo2: List3 = {
     id: 2,
     title: '할일2',
     content: '할일2',
@@ -33,7 +33,7 @@
     updateAt: new Date(),
   }
 
-  console.log(todo1)
-  console.log(todo2)
+  console.log("todo1:", todo1);
+  console.log("todo2:", todo2);
 })();
 
