334b12781e9e5f392a75a062ba372606615155bc
5647kr <5647kr@gmail.com>
2025-05-03 12:00:00 +0900

    TypeScript const Enum

--- TypeScript/ch01/ex01-22 const Enum.ts
+++ TypeScript/ch01/ex01-22 const Enum.ts
@@ -1,17 +1,17 @@
-(()=>{
-  console.log("==========const Enum==========");
+(() => {
+  console.log("========== const Enum ==========");
 
   //* Enum: 상수 데이터들의 집합을 정의
   const enum ClassName {
-    JS = 'javascript',
-    TS = 'typescript', 
-    React = 'react'
+    JS = "javascript",
+    TS = "typescript",
+    React = "react",
   }
 
   function getSchedule(subject: ClassName): string | undefined {
-    if(ClassName.JS === subject) {
+    if (ClassName.JS === subject) {
       return `${ClassName.JS} 수업은 4일 교육, 2일 프로젝트`;
-    }else if(ClassName.TS === subject) {
+    } else if (ClassName.TS === subject) {
       return `${ClassName.TS} 수업은 3일 교육, 1일 프로젝트`;
     }
   }
@@ -19,6 +19,6 @@
   const jsSchedule = getSchedule(ClassName.JS);
   const tsSchedule = getSchedule(ClassName.TS);
 
-  console.log(jsSchedule)
-  console.log(tsSchedule)
+  console.log("jsSchedule:", jsSchedule);
+  console.log("tsSchedule:", tsSchedule);
 })();
