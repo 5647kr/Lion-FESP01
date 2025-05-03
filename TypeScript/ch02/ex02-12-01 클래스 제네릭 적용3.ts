1a410eebb71397157a93333ebd395adaeab7a9e7
5647kr <5647kr@gmail.com>
2025-05-03 12:00:00 +0900

    TypeScript 클래스 제네릭 적용3

---
+++ TypeScript/ch02/ex02-12-01 클래스 제네릭 적용3.ts
@@ -0,0 +1,38 @@
+(()=>{
+  console.log("========== 클래스에 제네릭 적용 ==========");
+
+  interface TodoInfoDetail {
+    id: number;
+    title: string;
+    content: string;
+    done: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  type TodoRegist = Pick<TodoInfoDetail, "title" | "content">;
+  type TodoInfo = Omit<TodoInfoDetail, "createdAt" | "updatedAt">;
+  type TodoList = Omit<TodoInfo, "content">;
+
+  var todo1: TodoRegist = {
+    title: "할일1",
+    content: "등록에 사용"
+  }
+
+  var todo2: TodoInfo = {
+    id: 2,
+    title: "할일2",
+    content: "상세조회에 사용",
+    done: true
+  }
+
+  var todo3: TodoList = {
+    id: 3,
+    title: "할일3",
+    done: true
+  }
+
+  console.log("todo1:", todo1)
+  console.log("todo2:", todo2)
+  console.log("todo3:", todo3)
+})();
