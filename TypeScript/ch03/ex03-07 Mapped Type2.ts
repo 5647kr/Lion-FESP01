e95f88e9f24c24ae162b067b4f5f88fb1b259589
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript Mapped Type2

---
+++ TypeScript/ch03/ex03-07 Mapped Type2.ts
@@ -0,0 +1,32 @@
+(()=>{
+  console.log("========== mapped type2 ==========");
+
+  type User = {
+    id: string,
+    name: string,
+    address: string,
+    phone: string,
+  }
+
+  type ArrUser = {
+    // [Prop in "id" | "name"]: string[]
+    [Prop in keyof User]: string[]
+  }
+
+  const kim: User = {
+    id: "1",
+    name: "김철수",
+    address: "서울시",
+    phone: "01012345678"
+  }
+
+  const lee: ArrUser = {
+    id: ["1"],
+    name: ["이수정"],
+    address: ["서울시"],
+    phone: ["01098765432"]
+  }
+
+  console.log("kim:", kim)
+  console.log("lee:", lee)
+})();
