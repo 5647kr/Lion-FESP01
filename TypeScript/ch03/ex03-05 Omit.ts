d0d19e06d5c372fed0e3ecd1a9f318f8a308366a
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript Omit

---
+++ TypeScript/ch03/ex03-05 Omit.ts
@@ -0,0 +1,40 @@
+(()=>{
+  console.log("========== Omit ==========");
+
+  // type의 모든 속성에서 keys를 제거한 타입을 생성한다.
+
+  interface TodoInfoDetail {
+    id: number;
+    title: string;
+    content: string;
+    done: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  type TodoRegist = Pick<TodoInfoDetail, "title" | "content">;
+  type TodoInfo = Omit<TodoInfoDetail, "createdAt" | "updatedAt">;
+  type TodoList = Omit<TodoInfo, "content">;
+
+  var todo1: TodoRegist = {
+    title: "할일1",
+    content: "등록에 사용"
+  }
+
+  var todo2: TodoInfo = {
+    id: 2,
+    title: "할일2",
+    content: "상세조회에 사용",
+    done: true
+  }
+
+  var todo3: TodoList = {
+    id: 3,
+    title: "할일3",
+    done: true
+  }
+
+  console.log("todo1:", todo1)
+  console.log("todo2:", todo2)
+  console.log("todo3:", todo3)
+})();
