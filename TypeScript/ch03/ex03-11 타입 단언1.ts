7d54c3e46187277168d89cce65d57229d2841315
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript 타입 단언

---
+++ TypeScript/ch03/ex03-11 타입 단언1.ts
@@ -0,0 +1,20 @@
+(() => {
+  console.log("========== 타입 단언1 ==========");
+
+  // var num1 = 10 as string; error
+
+  var num2 = 10 as any as string;
+
+  console.log("num2:", num2);
+
+  function numFuc1(num) {
+    return num;
+  }
+
+  var returnNum = numFuc1(3.141592) as number;
+  console.log("returnNum:", returnNum.toFixed(2));
+
+
+  var str1 = numFuc1("hello") as string;
+  console.log("str1:", str1.toUpperCase())
+})();
