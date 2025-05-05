802ca176b6bc34d328eb31f62f86602628a5d93f
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript ReadOnly

---
+++ TypeScript/ch03/ex03-01 ReadOnly.ts
@@ -0,0 +1,32 @@
+(() => {
+  console.log("========== ReadOnly ==========");
+
+  // type의 모든 속성을 readonly로 설정한 타입을 생성한다.
+
+  interface Todo {
+    title: string;
+    content: string;
+  }
+
+
+  // todo1
+  const todo1: Todo = {
+    title: "할일 1",
+    content: "내용 1",
+  };
+  
+  todo1.title = "수정 1";
+  
+  console.log("todo1:", todo1);
+
+
+  // todo2
+  const todo2: Readonly<Todo> = {
+    title: "할일 2",
+    content: "내용 2",
+  };
+
+  // todo2.title = "수정 1"; error
+
+  console.log("todo2:", todo2);
+})();
