1bced71f0ec4a8f50d16590767752b8664597c20
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript 매핑 수정자

---
+++ TypeScript/ch03/ex03-08 매핑 수정자.ts
@@ -0,0 +1,42 @@
+(() => {
+  console.log("========== 매핑 수정자 ==========");
+
+  interface Todo {
+    id: number;
+    title: string;
+    content: string;
+  }
+
+  type optionalTodo<T> = {
+    // 1. 하드 코딩
+    // id?: number;
+    // title?: string;
+    // content?: string;
+
+    // 2. mapped
+    // [p in "id" | "title" | "content"]?: Todo[p]
+
+    // 3. keyof
+    // [p in keyof Todo]?: Todo[p]
+
+    // 4. 제네릭
+    [P in keyof T]?: T[P]
+  };
+
+  // todo1
+  const todo1: Todo = {
+    id: 1,
+    title: "할일 1",
+    content: "내용 1",
+  };
+
+  console.log("todo1:", todo1);
+
+  // todo1
+  const todo2: optionalTodo<Todo> = {
+    title: "할일 2",
+    content: "내용 2",
+  };
+
+  console.log("todo2:", todo2);
+})();
