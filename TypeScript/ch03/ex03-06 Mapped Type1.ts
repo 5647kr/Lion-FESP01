9c0d732a3c1726b5268824562069f8f11083e66d
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript Mapped Type1

---
+++ TypeScript/ch03/ex03-06 Mapped Type1.ts
@@ -0,0 +1,18 @@
+(()=>{
+  console.log("========== mapped type1 ==========");
+
+  type Userfields = "id" | "name" | "address" | "phone"
+
+  type User = {
+    [field in Userfields]: string;
+  }
+
+  const kim: User = {
+    id: "1",
+    name: "김철수",
+    address: "서울시",
+    phone: "01012345678"
+  }
+
+  console.log(kim)
+})();
