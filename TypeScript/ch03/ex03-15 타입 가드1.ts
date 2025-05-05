10168935a054db90099bf327030db3cc16e033b2
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript 타입 가드1

---
+++ TypeScript/ch03/ex03-15 타입 가드1.ts
@@ -0,0 +1,26 @@
+(()=>{
+  console.log("========== 타입 가드1 ==========");
+
+  console.log("typeof 10:", typeof 10)
+  console.log("typeof hello:", typeof "hello")
+  console.log("typeof false:", typeof false)
+  console.log("typeof new Function():", typeof new Function())
+  console.log("typeof undefined:", typeof undefined)
+  console.log("typeof null:", typeof null)
+  console.log("typeof {}:", typeof {})
+  console.log("typeof []:", typeof [])
+  console.log("typeof new Date():", typeof new Date())
+
+
+
+  function print(msg: string | number) {
+    if(typeof msg === "string") {
+      console.log("msg.length:", msg.length)
+    } else if(typeof msg === "number") {
+      console.log("msg.toFixed(2):", msg.toFixed(2))
+    }
+  }
+
+  print("hello")
+  print(3.14)
+})();
