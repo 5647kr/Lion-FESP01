a9a3fc88752bc6bcf7b6833addb92d853f0af52d
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript Partial

---
+++ TypeScript/ch03/ex03-03 Partial.ts
@@ -0,0 +1,47 @@
+(() => {
+  console.log("========== Partial ==========");
+
+  // type의 모든 속성을 옵셔널로 설정한 타입을 생성한다.(옵셔널 프로퍼티 추가)
+
+  interface Todo {
+    title: string;
+    content: string;
+  }
+
+  // todo1
+  const todo1: Todo = {
+    title: "할일 1",
+    content: "내용 1",
+  };
+
+  console.log("todo1:", todo1);
+
+  // todo2
+  const todo2: Partial<Todo> = {
+    title: "할일 2",
+  };
+
+  console.log("todo2:", todo2);
+
+
+  // 예시
+  const extodo1: Todo = {
+    title: "할일 1",
+    content: "내용 1",
+  };
+
+  const extodo2: Partial<Todo> = {
+    content: "내용 2",
+  };
+
+  const extodo3: Partial<Todo> = {
+    title: "할일 3",
+  };
+
+  function updateTodo(todo: Todo, newTodo: Partial<Todo>) {
+    return { ...todo, ...newTodo };
+  }
+
+  console.log(updateTodo(extodo1, extodo2));
+  console.log(updateTodo(extodo1, extodo3));
+})();
