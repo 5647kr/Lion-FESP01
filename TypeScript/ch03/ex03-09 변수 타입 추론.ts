918c4b363638c3e025c2c0c9f91df7b4efbb6cfd
5647kr <5647kr@gmail.com>
2025-05-05 12:00:00 +0900

    TypeScript 변수 타입 추론

---
+++ TypeScript/ch03/ex03-09 변수 타입 추론.ts
@@ -0,0 +1,32 @@
+(()=>{
+  console.log("========== 변수 타입 추론 ==========");
+
+  var str1 = "kim";
+  var num1 = 1234;
+  // 선언을 먼저 할 경우는 가능한 타입을 지정해야한다.
+  var undefined1: string;
+  // undefined1 = 100; error
+
+  // 지정 하지 않으면 any타입이 되어 typescript 를 사용하는 의미가 사라진다.
+  var undefined2;
+  undefined2 = "김철수";
+  undefined2 = 100;
+
+
+  interface Todo {
+    title: string, 
+    content: string,
+  }
+
+  // 오타가 있어도 따로 error표시를 하지 않는다.
+  const todo1 = {
+    titel: "할일 1",
+    content: "내용 1"
+  }
+
+  // 객체는 타입 별칭이나 인터페이스를 지정해야 안전하게 사용할 수 있다.
+  const todo2: Todo = {
+    title: "할일 1",
+    content: "내용 1"
+  }
+})();
