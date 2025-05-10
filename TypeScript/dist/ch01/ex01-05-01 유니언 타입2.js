655132a41ba49ed333a2271938373cf2dad536d5
5647kr <5647kr@gmail.com>
2025-05-10 22:21:07 +0900

    컴파일

--- TypeScript/dist/ch01/ex01-05-01 유니언 타입2.js
+++ TypeScript/dist/ch01/ex01-05-01 유니언 타입2.js
@@ -2,17 +2,18 @@
 (() => {
     console.log("========== 유니언 타입(union type)과 타입 가드 ==========");
     function fuc1(msg) {
-        if (typeof msg === 'string') { //* 타입 가드
+        if (typeof msg === "string") {
+            //* 타입 가드
             console.log(`${msg} 글자 수: ${msg.length}`);
         }
-        else if (typeof msg === 'number') {
+        else if (typeof msg === "number") {
             console.log(`${msg} + 10 = ${msg + 10}`);
         }
         else {
-            console.log(`${msg} : ${msg ? '참' : '거짓'}`);
+            console.log(`${msg} : ${msg ? "참" : "거짓"}`);
         }
     }
-    fuc1('world');
+    fuc1("world");
     fuc1(100);
     fuc1(false);
 })();
