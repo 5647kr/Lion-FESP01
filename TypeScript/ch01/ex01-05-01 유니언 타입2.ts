92102bcf746e59604695170e2ad82f840294d3ce
5647kr <5647kr@gmail.com>
2025-05-10 22:21:30 +0900

    TypeScript 유니언 타입2

--- TypeScript/ch01/ex01-05-01 유니언 타입2.ts
+++ TypeScript/ch01/ex01-05-01 유니언 타입2.ts
@@ -1,17 +1,18 @@
-(()=>{
+(() => {
   console.log("========== 유니언 타입(union type)과 타입 가드 ==========");
 
   function fuc1(msg: string | number | boolean): void {
-    if(typeof msg === 'string') { //* 타입 가드
-      console.log(`${msg} 글자 수: ${msg.length}`)
-    } else if(typeof msg === 'number') {
-      console.log(`${msg} + 10 = ${msg + 10}`)
+    if (typeof msg === "string") {
+      //* 타입 가드
+      console.log(`${msg} 글자 수: ${msg.length}`);
+    } else if (typeof msg === "number") {
+      console.log(`${msg} + 10 = ${msg + 10}`);
     } else {
-      console.log(`${msg} : ${msg ? '참' : '거짓'}`)
+      console.log(`${msg} : ${msg ? "참" : "거짓"}`);
     }
   }
 
-  fuc1('world')
-  fuc1(100)
-  fuc1(false)
+  fuc1("world");
+  fuc1(100);
+  fuc1(false);
 })();
