650c9b81effa85ec6fa36f9ee56f187b7697921f
5647kr <5647kr@gmail.com>
2025-05-13 10:17:49 +0900

    TypeScript 제네릭 일반함수1

--- TypeScript/ch02/ex02-07 제네릭 일반함수1.ts
+++ TypeScript/ch02/ex02-07 제네릭 일반함수1.ts
@@ -4,14 +4,10 @@
   // 함수를 생성할 때 함수에서 사용할 매개변수, 리턴 타입을 정의하지 않고 호출하는 시점에 원하는 타입을 지정해서 사용한다.
 
   // 제네릭 사용 이전
-  function fuc1(msg: string): string {
-    return msg;
-  }
-
-  function fuc2(msg: number): number {
+  function fuc1(msg: string | number): string | number {
     return msg;
   }
 
   console.log(fuc1("hello"));
-  console.log(fuc2(100));
+  console.log(fuc1(100));
 })();
