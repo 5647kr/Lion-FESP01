f2ccdebac0c6c39a1d8bbca025c8acc7de1be3e3
5647kr <5647kr@gmail.com>
2025-05-14 10:08:25 +0900

    TypeScript 타입 단언3

--- TypeScript/ch03/ex03-13 타입 단언3.ts
+++ TypeScript/ch03/ex03-13 타입 단언3.ts
@@ -1,5 +1,5 @@
 // ex03-13.ts 복사
-(()=>{
+(() => {
   console.log("========== 타입 단언3 ==========");
 
   // {} 타입 추론
@@ -8,7 +8,6 @@
 
   var todo1 = {} as Todo;
   todo1.title = "할일 1";
-
 
   // {title: string} 타입 추론
   // var todo2 = {
@@ -23,7 +22,6 @@
 
   todo2.content = "내용 2";
 
-
   var todo3 = {
     title: "할일 3",
     content: "내용 3",
@@ -34,19 +32,17 @@
   console.log("todo2:", todo2);
   console.log("todo3:", todo3);
 
-
-
   // 처음부터 TS로 만들면,
 
   interface Todo {
-    title: string,
+    title: string;
     content: string;
   }
 
   const todo4: Todo = {
     title: "할일 4",
-    content: "내용 4"
-  }
+    content: "내용 4",
+  };
 
   console.log("todo4:", todo4);
 })();
