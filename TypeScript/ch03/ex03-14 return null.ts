96716e478fe34263c92965538136cb3a160f1cc5
5647kr <5647kr@gmail.com>
2025-05-14 10:08:31 +0900

    TypeScript return null

--- TypeScript/ch03/ex03-14 return null.ts
+++ TypeScript/ch03/ex03-14 return null.ts
@@ -1,13 +1,14 @@
-(()=>{
+(() => {
   var div = document.querySelector("div");
   // div.innerHTML = "hello"; error
 
   // !를 붙여주면 non null assertion
   // div!.innerHTML = "hello";
 
-  if(div !== null) {  // 타입 가드
-    div.innerHTML = "Hello"
+  if (div !== null) {
+    // 타입 가드
+    div.innerHTML = "Hello";
   }
 
-  console.log(div)
+  console.log(div);
 })();
