b7699d23100dfe3088247b4da1f750d5f59c80d1
5647kr <5647kr@gmail.com>
2025-05-15 08:51:25 +0900

    TypeScript 속성 값 체크1

--- TypeScript/ch03/ex03-18 속성 값 체크1.ts
+++ TypeScript/ch03/ex03-18 속성 값 체크1.ts
@@ -1,29 +1,29 @@
-(()=>{
+(() => {
   console.log("========== 속성 값 체크 ==========");
 
   const kim: User = {
     name: "김철수",
     age: 30,
-    nickname: "철수"
-  }
+    nickname: "철수",
+  };
 
-  const lee:UnknownUser = {
+  const lee: UnknownUser = {
     name: "이영희",
-    age: "비공개"
-  }
+    age: "비공개",
+  };
 
-  const hong:Admin = {
-    name: "홍길동"
-  }
+  const hong: Admin = {
+    name: "홍길동",
+  };
 
   function printUser(user: User | UnknownUser | Admin) {
     // User 타입일 경우 나이 출력
-    if("age" in user && typeof user.age === "number") {
-      console.log((user as User).nickname)
+    if ("age" in user && typeof user.age === "number") {
+      console.log((user as User).nickname);
     }
   }
 
-  printUser(kim)
-  printUser(lee)
-  printUser(hong)
+  printUser(kim);
+  printUser(lee);
+  printUser(hong);
 })();
