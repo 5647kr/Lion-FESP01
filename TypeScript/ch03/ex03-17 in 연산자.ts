edbbd418bbf6e3a4f8b2a331e177f5a595d5f65c
5647kr <5647kr@gmail.com>
2025-05-15 08:51:17 +0900

    TypeScript in 연산자

--- TypeScript/ch03/ex03-17 in 연산자.ts
+++ TypeScript/ch03/ex03-17 in 연산자.ts
@@ -1,7 +1,7 @@
 // ex03-04.ts 복사
-(()=>{
+(() => {
   console.log("========== in 연산자 ==========");
-  
+
   interface TodoInfoDetail {
     id: number;
     title: string;
@@ -17,34 +17,34 @@
 
   var todo1: TodoRegist = {
     title: "할일1",
-    content: "등록에 사용"
-  }
+    content: "등록에 사용",
+  };
 
   var todo2: TodoInfo = {
     id: 2,
     title: "할일2",
     content: "상세조회에 사용",
-    done: true
-  }
+    done: true,
+  };
 
   var todo3: TodoList = {
     id: 3,
     title: "할일3",
-    done: true
-  }
+    done: true,
+  };
 
   function printTodo(todo: TodoRegist | TodoInfo) {
-    if("id" in todo) {
-      console.log(`[TodoInfo] id: ${todo.id}, title ${todo.title}`)
+    if ("id" in todo) {
+      console.log(`[TodoInfo] id: ${todo.id}, title ${todo.title}`);
     } else {
-      console.log(`[TodoRegist] title ${todo.title}`)
+      console.log(`[TodoRegist] title ${todo.title}`);
     }
   }
 
-  printTodo(todo1)
-  printTodo(todo2)
+  printTodo(todo1);
+  printTodo(todo2);
 
-  console.log("todo1:", todo1)
-  console.log("todo2:", todo2)
-  console.log("todo3:", todo3)
+  console.log("todo1:", todo1);
+  console.log("todo2:", todo2);
+  console.log("todo3:", todo3);
 })();
